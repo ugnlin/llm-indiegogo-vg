a7102cc3aa3f980795d7ee0971bd8ecf7a2be10e
ugnlin <eugene.lin253@gmail.com>
2023-12-19 09:19:39 +0000

    handle no money raised

--- src/common/rawfeatures.py
+++ src/common/rawfeatures.py
@@ -19,5 +19,9 @@ class RawFeatures:
         kwargs['open_date'] = datetime.fromisoformat(kwargs.pop('open_date'))
         kwargs['close_date'] = datetime.fromisoformat(kwargs.pop('close_date'))
         kwargs['ended'] = True if datetime.now(kwargs['close_date'].tzinfo) > kwargs['close_date'] else False
-        kwargs['goal'] = kwargs['raised'] * kwargs.pop('raised_percent')
+        if kwargs['raised_percent'] > 0:
+            kwargs['goal'] = kwargs['raised'] / kwargs.pop('raised_percent')
+        else:
+            kwargs.pop('raised_percent')
+            kwargs['goal'] = -1
         return cls(**kwargs)
