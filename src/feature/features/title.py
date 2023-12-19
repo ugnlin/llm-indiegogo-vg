a17dfb263c790d484645803a7dacfeba6aa10427
ugnlin <eugene.lin253@gmail.com>
2023-12-19 11:57:14 +0000

    title constructor

--- src/feature/features/title.py
+++ src/feature/features/title.py
@@ -6,4 +6,28 @@ class Title:
         self.value = {
             'title_chars': len(raw_data.name),
         }
-        llama(raw_data.name)
+
+        res = llama(f"""
+        Here is a crowdfunding campaign title: '{raw_data.name}'.
+        
+        What is the length of the title in words? Does it seem snappy? Does it include a tagline?
+        """)
+
+        formatted_res = llama(f"""
+        Please format the following into json, and return the json only. The values should match that of the example
+        format given, and if unsure, should occupy the string value 'null'.
+        
+        Format:
+        {{
+            'title_words': <number of words in the title>,
+            'snappy': <bool>,
+            'tagline: <bool>,
+        }}
+        
+        Raw text:
+        {res}
+        
+        Json:
+        """)
+
+        self.value.update(formatted_res)
